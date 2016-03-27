45812382d90b1db6ace9287b02953f0915ce6390
arcady <arcady@github.com>
2016-03-27 17:07:23 +0200

    fix graphics install

--- scripts/build/graphics.sh
+++ scripts/build/graphics.sh
@@ -13,13 +13,6 @@ do_graphics_get() {
     # DFGraphics set from Github
 	CT_GetGit "graphics" "${CT_GRAPHICS_VERSION}" \
 			  https://github.com/DFgraphics/DFgraphics.git
-	
-	# if [ "${CT_IRONHAND}" = "y" ]; then
-	# 	do_ironhand_get
-	# fi
-	# if [ "${CT_PHOEBUS}" = "y" ]; then
-	# 	do_phoebus_get
-	# fi	
 }
 
 # Extract
@@ -30,18 +23,11 @@ do_graphics_extract() {
 
 # Extract
 do_graphics_build() {
-	lnp_dir=${CT_SRC_DIR}/lnp-${CT_LNP_VERSION}/LNP
-	if [ -d "$lnp_dir" ]; then
-		cd $lnp_dir
-		CT_Extract nochdir "graphics-${CT_GRAPHICS_VERSION}"
-		CT_DoExecLog ALL rm -fr Graphics
-		CT_DoExecLog ALL ln -s "graphics-${CT_GRAPHICS_VERSION}" Graphics
-		if [ "${CT_GRAPHICS_REV}" != "" ]; then
-			CT_DoLog INFO "Patching DFGraphics manifests to allow use with DF-${CT_DF_VERSION}"
-			sed -i -e 's/\"df_min_version\": \".*\"/\"df_min_version\": \"0.00"/g' -e 's/\"df_max_version\": \".*\"/\"df_max_version\": \"1.00"/g' Graphics/*/manifest.json
-		fi
-		cd -
-	else
-		CT_Abort "LNP directory $lnp_dir not found!"
-	fi
+	CT_Pushd "$(get_lnp_dir)/LNP"
+	CT_Extract nochdir "graphics-${CT_GRAPHICS_VERSION}"
+	CT_DoExecLog ALL rm -fr Graphics
+	CT_DoExecLog ALL ln -s "graphics-${CT_GRAPHICS_VERSION}" Graphics
+	CT_DoLog INFO "Patching DFGraphics manifests to allow use with DF-${CT_DF_VERSION}"
+	sed -i -e 's/\"df_min_version\": \".*\"/\"df_min_version\": \"0.00"/g' -e 's/\"df_max_version\": \".*\"/\"df_max_version\": \"1.00"/g' Graphics/*/manifest.json
+	CT_Popd
 }
