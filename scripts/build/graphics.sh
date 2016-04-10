5bc915d7ecf18cb36bac87bf027297bc1c17c707
McArcady <arcady@github.com>
2016-04-10 22:36:32 +0200

    adapt to new dir structure in dfgraphics repo

--- scripts/build/graphics.sh
+++ scripts/build/graphics.sh
@@ -7,9 +7,6 @@ get_graphics_dir() {
 
 # Download
 do_graphics_get() {
-	if [ "${CT_GRAPHICS_VERSION}" = "" ]; then
-		export CT_GRAPHICS_VERSION="${CT_GRAPHICS_REV}"
-	fi
     # DFGraphics set from Github
 	CT_GetGit "graphics" "${CT_GRAPHICS_VERSION}" \
 			  https://github.com/DFgraphics/DFgraphics.git
@@ -26,7 +23,7 @@ do_graphics_build() {
 	CT_Pushd "$(get_lnp_dir)/LNP"
 	CT_Extract nochdir "graphics-${CT_GRAPHICS_VERSION}"
 	CT_DoExecLog ALL rm -fr Graphics
-	CT_DoExecLog ALL ln -s "graphics-${CT_GRAPHICS_VERSION}" Graphics
+	CT_DoExecLog ALL ln -s "graphics-${CT_GRAPHICS_VERSION}/graphics-packs" Graphics
 	CT_DoLog INFO "Patching DFGraphics manifests to allow use with DF-${CT_DF_VERSION}"
 	sed -i -e 's/\"df_min_version\": \".*\"/\"df_min_version\": \"0.00"/g' -e 's/\"df_max_version\": \".*\"/\"df_max_version\": \"1.00"/g' Graphics/*/manifest.json
 	CT_Popd
