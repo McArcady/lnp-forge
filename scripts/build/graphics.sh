3e1fc5f03f2922780cc4bb6b2bab5d27086ee5e1
McArcady <McArcady@github.com>
2016-08-13 11:59:23 +0200

    graphics: fix extraction directory

--- scripts/build/graphics.sh
+++ scripts/build/graphics.sh
@@ -13,16 +13,17 @@ do_graphics_get() {
 
 # Extract
 do_graphics_extract() {
-	CT_Pushd "$(get_lnp_dir)/LNP"
-	CT_Extract nochdir "graphics-${CT_GRAPHICS_VERSION}"
-	CT_Popd
+	# extract in src dir
+	CT_Extract "graphics-${CT_GRAPHICS_VERSION}"
 }
 
 # Build
 do_graphics_build() {
 	CT_Pushd "$(get_lnp_dir)/LNP"
+    # copy to DF dir
 	CT_DoExecLog ALL rm -fr Graphics
-	CT_DoExecLog ALL ln -s "graphics-${CT_GRAPHICS_VERSION}/graphics-packs" Graphics
+	CT_DoExecLog ALL cp -fR "${CT_SRC_DIR}/graphics-${CT_GRAPHICS_VERSION}/graphics-packs" "Graphics"
+	# patch manifests
 	sed -i -e 's/\"df_min_version\": \".*\"/\"df_min_version\": \"0.00"/g' -e 's/\"df_max_version\": \".*\"/\"df_max_version\": \"1.00"/g' Graphics/*/manifest.json
 	CT_Popd
 }
