0d8ffd2d481c88025a66236928b481e8552759cd
McArcady <McArcady@github.com>
2016-11-09 22:42:32 +0100

    dfhack: add v0.43.05-alpha1 twbt: add v5.66

--- scripts/build/dfhack.sh
+++ scripts/build/dfhack.sh
@@ -21,7 +21,7 @@ do_dfhack_build() {
 	df_dir=${CT_SRC_DIR}/lnp-${CT_LNP_VERSION}/df_${CT_DF_VERSION}
 
 	CT_Pushd "${dfhack_src_dir}/build"
-    CT_DoExecLog ALL cmake .. -DCMAKE_INSTALL_PREFIX=${df_dir}
+    CT_DoExecLog ALL cmake .. -DCMAKE_INSTALL_PREFIX=${df_dir} -DBUILD_STONESENSE=1
 	CT_DoExecLog ALL make ${JOBSFLAGS} install
  	CT_Popd
 }
