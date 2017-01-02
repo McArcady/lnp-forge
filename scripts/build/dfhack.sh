7accd29eae682b6b42e6740547e5476511026ad1
McArcady <McArcady@github.com>
2017-01-02 21:40:04 +0100

    dfhack: add version 0.43.05-alpha4 twbt: add version 5.70 df: fix 64-bit df start-up

--- scripts/build/dfhack.sh
+++ scripts/build/dfhack.sh
@@ -20,9 +20,17 @@ do_dfhack_build() {
 	dfhack_src_dir="${CT_SRC_DIR}/dfhack-${CT_DFHACK_VERSION}"
 	df_dir=${CT_SRC_DIR}/lnp-${CT_LNP_VERSION}/df_${CT_DF_VERSION}
 
+	# 64-bit compile?
+	if [ "${CT_DFHACK_VERSION}" \> "0.43.05" ]; then
+		extra_args=" -DDFHACK_BUILD_ARCH=64"
+	else
+		extra_args=" -DBUILD_STONESENSE=1"
+	fi
+	
 	CT_Pushd "${dfhack_src_dir}/build"
-    CT_DoExecLog ALL cmake .. -DCMAKE_INSTALL_PREFIX=${df_dir} -DBUILD_STONESENSE=1
+    CT_DoExecLog ALL cmake .. -DCMAKE_BUILD_TYPE:string=Release -DCMAKE_INSTALL_PREFIX=${df_dir} ${extra_args}
 	CT_DoExecLog ALL make ${JOBSFLAGS} install
+	CT_DoExecLog ALL rm -f "${df_dir}/libs/libstdc++.so.6"
  	CT_Popd
 }
 
