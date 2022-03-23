59c4d00e06e55c2181980d9767d347b8a1930966
McArcady <mcarcady@github.com>
2022-03-23 22:51:30 +0100

    menuconfig: add options to deactivate dfhack plugins * deactivate kloker

--- scripts/build/twbt.sh
+++ scripts/build/twbt.sh
@@ -32,6 +32,8 @@ do_twbt_extract() {
 		CT_DoExecLog ALL ln -s twbt-${CT_TWBT_VERSION} twbt
 		# patch
 		CT_Patch nochdir "twbt" "${CT_TWBT_VERSION}"
+		# activate
+		echo "add_subdirectory(twbt)" >> ${dfhack_plugins_dir}/CMakeLists.custom.txt
 		CT_Popd
 	else
 		CT_Abort "DFHack plugins directory $dfhack_plugins_dir not found!"
